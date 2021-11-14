71afe5f1ca804398efd9e9b7990a0e5cf6095e7c
mrmortis <saurus.48@mail.ru>
2021-11-14 16:49:27 +0300

    feat(components): create moderator panel layout

---
+++ src/components/ModeratorPanel.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <HeaderAuthModer/>
+    <ItemListGames/>
+    <ItemListGames/>
+    <ItemListGames/>
+  </div>
+</template>
+
+<script>
+import HeaderAuthModer from "./headers/HeaderAuthModer";
+import ItemListGames from "./templates/ItemListGames";
+
+export default {
+  name: "ModeratorPanel",
+  components:{ItemListGames, HeaderAuthModer},
+
+}
+</script>
+
+<style scoped>
+
+</style>
