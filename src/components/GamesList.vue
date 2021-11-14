9a911d99d1df74547e7a6ee56746640c294ec10a
mrmortis <saurus.48@mail.ru>
2021-11-14 16:50:18 +0300

    feat(components): create games list layout

---
+++ src/components/GamesList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ItemListGames class="item"/>
+    <ItemListGames class="item"/>
+    <ItemListGames class="item"/>
+  </div>
+</template>
+
+<script>
+import ItemListGames from "./templates/ItemListGames";
+export default {
+  name: "GamesList",
+  components: {
+    ItemListGames,
+  }
+}
+</script>
+
+<style scoped>
+  div{
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+  .item{
+    flex-basis: 500px;
+  }
+
+</style>
