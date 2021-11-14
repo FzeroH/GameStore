443f9bcd1d479a07f45876fd6471d5a5a4992000
mrmortis <saurus.48@mail.ru>
2021-11-14 16:30:44 +0300

    feat(components): create main page layout

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,16 +1,25 @@
 <template>
   <div>
-
+    <HeaderUnAuth/>
+    <ListGames class="list-game"/>
   </div>
 </template>
 
 <script>
+import HeaderUnAuth from "./headers/HeaderUnAuth";
+import ListGames from "./ListGames";
+
 export default {
   name: 'MainPage',
+  components: {
+    ListGames,
+    HeaderUnAuth},
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+
 
 </style>
